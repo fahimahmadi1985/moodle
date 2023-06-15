809c07ca404be2b3741446c53a9bcbd36907f0a1
AbudiMohamed <a.hossni94@gmail.com>
2023-06-15 18:24:19 +0200

    adjust server to keep the 404 page while updating

--- deployment-scripts/after-install.sh
+++ deployment-scripts/after-install.sh
@@ -3,6 +3,8 @@
 sudo apt-get update
 sudo -u www-data php8.2 /var/www/html/moodle-vPlus/admin/cli/upgrade.php
 sudo cp /var/www/html/config.php /var/www/html/moodle-vPlus
+sudo cp /var/www/html/404.html /var/www/html/moodle-vPlus
+sudo cp /var/www/html/404.svg /var/www/html/moodle-vPlus
 sudo rm -rf /var/www/html/moodle
 sudo mv /var/www/html/moodle-vPlus /var/www/html/moodle
 sudo chown -R root:root /var/www/html/moodle
--- deployment-scripts/before-install.sh
+++ deployment-scripts/before-install.sh
@@ -4,3 +4,5 @@ sudo -u www-data php8.2 /var/www/html/moodle/admin/cli/maintenance.php --enable
 sudo apt-get update
 sudo makdir moodle-vPlus
 sudo cp /var/www/html/moodle/config.php /var/www/html/config.php
+sudo cp /var/www/html/moodle/404.html /var/www/html/404.html
+sudo cp /var/www/html/moodle/404.svg /var/www/html/404.svg
