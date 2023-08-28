81fddbb3a2f83b904593fde8991de59923b305a1
galymax <werkhowski@gmail.com>
2023-08-28 23:31:49 +0200

    fix parameters

--- moodle/local/get_bbb_events/externallib.php
+++ moodle/local/get_bbb_events/externallib.php
@@ -4,20 +4,35 @@ require_once($CFG->libdir . '/externallib.php');
 
 class local_getbbb_external extends external_api
 {
+    /**
+     * Returns description of method parameters
+     * @return external_function_parameters
+     */
     public static function get_data_parameters()
     {
         return new external_function_parameters(
             array(
-                'course_id' => new external_value(PARAM_INT, 'course_id', VALUE_OPTIONAL), // Marked as optional
-                'start_date' => new external_value(PARAM_INT, 'start_date', VALUE_OPTIONAL),
-                'end_date' => new external_value(PARAM_INT, 'end_date', VALUE_OPTIONAL),
+                'course_id' => new external_value(PARAM_INT, 'ID of the course', VALUE_OPTIONAL, null),
+                'start_date' => new external_value(PARAM_INT, 'Start date for filtering', VALUE_OPTIONAL, null),
+                'end_date' => new external_value(PARAM_INT, 'End date for filtering', VALUE_OPTIONAL, null)
             )
         );
     }
 
-    public static function get_data($course_id = null, $start_date = null, $end_date = null)
+    /**
+     * The function itself
+     * @param int $course_id
+     * @param int $start_date
+     * @param int $end_date
+     * @return array
+     */
+    public static function get_data()
     {
         global $DB;
+
+        $course_id = optional_param('course_id', null, PARAM_INT);
+        $start_date = optional_param('start_date', null, PARAM_INT);
+        $end_date = optional_param('end_date', null, PARAM_INT);
 
         $query = "SELECT 
             logs.id, logs.eventname, bbb.log AS bbb_log, logs.action, logs.crud,
@@ -37,31 +52,42 @@ class local_getbbb_external extends external_api
         AND 
             logs.target = 'meeting'";
 
-        $params = array();
+        $conditions = [];
+        $params = [];
 
         //CHECK IF course_id is provided
-        if (!is_null($course_id)) {
-            $query .= ' AND logs.courseid = :course_id';
+        if (!is_null($course_id) && $course_id > 0) {
+            $conditions[] = 'logs.courseid = :course_id';
             $params['course_id'] = $course_id;
         }
 
         //CHECK IF start_date is provided
-        if (!is_null($start_date)) {
-            $query .= ' AND logs.timecreated >= :start_date';
+        if (!is_null($start_date) && $start_date > 0) {
+            $conditions[] = 'logs.timecreated >= :start_date';
             $params['start_date'] = $start_date;
         }
 
         //CHECK IF end_date is provided
-        if (!is_null($end_date)) {
-            $query .= ' AND logs.timecreated <= :end_date';
+        if (!is_null($end_date) && $end_date > 0) {
+            $conditions[] = 'logs.timecreated <= :end_date';
             $params['end_date'] = $end_date;
         }
+
+        // Add conditions to the main query
+        if (count($conditions) > 0) {
+            $query .= ' AND ' . implode(' AND ', $conditions);
+        }
+
 
         $data = $DB->get_records_sql($query, $params);
 
         return $data;
     }
 
+    /**
+     * Returns description of method result value
+     * @return external_description
+     */
     public static function get_data_returns()
     {
         return new external_multiple_structure(
@@ -80,7 +106,7 @@ class local_getbbb_external extends external_api
                     'crs_fullname' => new external_value(PARAM_TEXT, 'crs_fullname'),
                     'crs_idnumber' => new external_value(PARAM_TEXT, 'crs_idnumber'),
                     'timecreated' => new external_value(PARAM_INT, 'timecreated'),
-                    'ip' => new external_value(PARAM_TEXT, 'ip'),
+                    'ip' => new external_value(PARAM_TEXT, 'ip')
                 )
             )
         );
--- moodle/local/get_bbb_events/version.php
+++ moodle/local/get_bbb_events/version.php
@@ -1,8 +1,8 @@
 <?php
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version   = 2023082301;            // The current plugin version (Date: YYYYMMDDXX).
-$plugin->release   = 2023082301;            // Same as version.
+$plugin->version   = 2023082801;            // The current plugin version (Date: YYYYMMDDXX).
+$plugin->release   = 2023082801;            // Same as version.
 $plugin->component = 'local_get_bbb_events';  // Full name of the plugin (used for diagnostics).
 //$plugin->requires = 2023082300;             // Requires this Moodle version.
 $plugin->maturity = MATURITY_STABLE;
